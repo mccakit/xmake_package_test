c55372772f1d5d22b0ddd095eab93f138ff9f45d
mccakit <117523238+mccakit@users.noreply.github.com>
2025-04-19 12:12:51 +0300

    init

---
+++ build/.deps/foo/msys/x86_64/release/src/lib/foo/foo.cpp.o.d
@@ -0,0 +1,21 @@
+{
+    values = {
+        [[C:\Users\cakit\scoop\apps\msys2\2025-02-21\clang64\bin\clang.exe]],
+        {
+            "-Qunused-arguments",
+            "-m64",
+            "-fvisibility=hidden",
+            "-fvisibility-inlines-hidden",
+            "-O3",
+            [[-Isrc\inc\foo]],
+            [[-Ibuild\.gens\foo\msys\x86_64\release\platform\windows\idl]],
+            "-DNDEBUG"
+        }
+    },
+    depfiles_format = "gcc",
+    depfiles = "build\\.objs\\foo\\msys\\x86_64\\release\\src\\lib\\foo\\__cpp_foo.cpp.cpp:   src/lib/foo/foo.cpp src/inc/foo/foo.h\
+",
+    files = {
+        [[src\lib\foo\foo.cpp]]
+    }
+}
